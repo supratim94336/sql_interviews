250cd96afcba27549ba8a6d40d512bbb3e8239e7
Supratim Das <supratim.gre2014@yahoo.com>
2021-02-12 16:03:09 +0100

    Update min_meeting_rooms.sql

--- min_meeting_rooms.sql
+++ min_meeting_rooms.sql
@@ -1,14 +1,14 @@
 -- postgres
 -- problem: Min number of meetings needed in order to suffice all meeting timings
 -- input should look like
---
+-- ______________________________
 -- | id | start_time | end_time |
 -- ______________________________
 -- |  1 |   02:00:00 | 02:59:59 |  
 -- |  2 |   03:00:00 | 03:59:59 |   
 -- |  3 |   04:00:00 | 04:59:59 |   
 -- |  4 |   03:00:00 | 04:59:59 |   
---
+-- ______________________________
 select max(overlap) as solution from (
     select count(t) as overlap from (
         select * from (
