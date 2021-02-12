e42bd18cd55279d729c6d22211f032e018752ace
Supratim Das <supratim.gre2014@yahoo.com>
2021-02-12 16:10:18 +0100

    create temp table in redshift

--- rs_temp_table.sql
+++ rs_temp_table.sql
@@ -1,8 +1,21 @@
-CREATE TEMP TABLE if not exists meetings6(id int, start_time timestamp, end_time timestamp);
-insert into meetings6 values
-(1,TO_TIMESTAMP('2021-02-07 08:00:00', 'YYYY-MM-DD HH24:MI:SS'),TO_TIMESTAMP('2021-02-07 09:15:59', 'YYYY-MM-DD HH24:MI:SS')),
-(2,TO_TIMESTAMP('2021-02-07 13:20:00', 'YYYY-MM-DD HH24:MI:SS'),TO_TIMESTAMP('2021-02-07 15:20:59', 'YYYY-MM-DD HH24:MI:SS')),
-(3,TO_TIMESTAMP('2021-02-07 10:00:00', 'YYYY-MM-DD HH24:MI:SS'),TO_TIMESTAMP('2021-02-07 14:00:59', 'YYYY-MM-DD HH24:MI:SS')),
-(4,TO_TIMESTAMP('2021-02-07 13:55:00', 'YYYY-MM-DD HH24:MI:SS'),TO_TIMESTAMP('2021-02-07 16:25:59', 'YYYY-MM-DD HH24:MI:SS')),
-(5,TO_TIMESTAMP('2021-02-07 14:00:00', 'YYYY-MM-DD HH24:MI:SS'),TO_TIMESTAMP('2021-02-07 17:45:59', 'YYYY-MM-DD HH24:MI:SS')),
-(6,TO_TIMESTAMP('2021-02-07 14:05:00', 'YYYY-MM-DD HH24:MI:SS'),TO_TIMESTAMP('2021-02-07 17:45:59', 'YYYY-MM-DD HH24:MI:SS'));
+-- redshift
+-- create dataset for min_meeting_rooms.sql problem
+-- output should look like
+-- __________________________________________________
+-- | id |      start_time     |       end_time      |
+-- __________________________________________________
+-- |  1 | 2021-02-07 08:00:00 | 2021-02-07 09:15:59 |  
+-- |  2 | 2021-02-07 13:20:00 | 2021-02-07 15:20:59 |   
+-- |  3 | 2021-02-07 10:00:00 | 2021-02-07 14:00:59 |   
+-- |  4 | 2021-02-07 13:55:00 | 2021-02-07 16:25:59 |   
+-- |  5 | 2021-02-07 14:00:00 | 2021-02-07 17:45:59 | 
+-- |  6 | 2021-02-07 14:05:00 | 2021-02-07 17:45:59 | 
+-- __________________________________________________
+create temp table if not exists meetings(id int, start_time timestamp, end_time timestamp);
+insert into meetings values
+(1,to_timestamp('2021-02-07 08:00:00', 'YYYY-MM-DD HH24:MI:SS'),to_timestamp('2021-02-07 09:15:59', 'YYYY-MM-DD HH24:MI:SS')),
+(2,to_timestamp('2021-02-07 13:20:00', 'YYYY-MM-DD HH24:MI:SS'),to_timestamp('2021-02-07 15:20:59', 'YYYY-MM-DD HH24:MI:SS')),
+(3,to_timestamp('2021-02-07 10:00:00', 'YYYY-MM-DD HH24:MI:SS'),to_timestamp('2021-02-07 14:00:59', 'YYYY-MM-DD HH24:MI:SS')),
+(4,to_timestamp('2021-02-07 13:55:00', 'YYYY-MM-DD HH24:MI:SS'),to_timestamp('2021-02-07 16:25:59', 'YYYY-MM-DD HH24:MI:SS')),
+(5,to_timestamp('2021-02-07 14:00:00', 'YYYY-MM-DD HH24:MI:SS'),to_timestamp('2021-02-07 17:45:59', 'YYYY-MM-DD HH24:MI:SS')),
+(6,to_timestamp('2021-02-07 14:05:00', 'YYYY-MM-DD HH24:MI:SS'),to_timestamp('2021-02-07 17:45:59', 'YYYY-MM-DD HH24:MI:SS'));
