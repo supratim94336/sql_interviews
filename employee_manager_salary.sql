32837000018184292da56ab7de9504977d5e076b
Supratim Das <supratim.gre2014@yahoo.com>
2021-02-28 20:59:55 +0100

    Update employee_manager_salary.sql

--- employee_manager_salary.sql
+++ employee_manager_salary.sql
@@ -14,20 +14,17 @@ with recursive t1 as ( -- traverse employees all the way up in the manager chain
   select id, name, salary, manager, salary indsalary, manager indirect, 1 as level
   from emp 
   union all
-    select t1.id, t1.name, t1.salary, e.manager, e.salary, t1.manager, t1.level + 1
-    from emp e, t1 
-    where t1.manager = e.id
-)
-, t2 as ( -- check salary condition more than equals to two
+  select t1.id, t1.name, t1.salary, e.manager, e.salary, t1.manager, t1.level + 1
+  from emp e, t1 
+  where t1.manager = e.id
+), t2 as ( -- check salary condition more than equals to two
   select *
   from t1
   where  indsalary >= 2 * salary
-)
-, t3 as ( -- get the row numbers 
+), t3 as ( -- get the row numbers 
   select t2.*, row_number() over (partition by id order by level) rn  
   from t2
-)
-, t4 as ( -- retain only first row per emp
+), t4 as ( -- retain only first row per emp
   select * from t3
   where rn = 1
 )
