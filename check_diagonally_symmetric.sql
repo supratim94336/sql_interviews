85250734f4a6e7a880905aaee46847338b40be05
Supratim Das <supratim.gre2014@yahoo.com>
2021-12-16 04:59:19 +0100

    description added

--- check_diagonally_symmetric.sql
+++ check_diagonally_symmetric.sql
@@ -1,6 +1,17 @@
 /*
-Find diagonally symmetric pairs: MySQL
+Find diagonally symmetric pairs
 */
+-- MySQL
+-- Result 1, 2
+-- _____________
+-- |  x |    y |
+-- _____________
+-- |  1 |    2 |
+-- |  2 |    1 |
+-- |  4 |    3 |
+-- |  5 |    6 |
+-- ________________________________
+
 SELECT A.x, A.y
 FROM FUNCTIONS A JOIN FUNCTIONS B ON
     A.x = B.y AND A.y = B.x
