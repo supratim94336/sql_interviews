8e2b567057f2d78059e21461f69d33fa22681b1f
Supratim Das <supratim.gre2014@yahoo.com>
2021-12-16 05:05:13 +0100

    description added

--- whose_bestfriend_earns_better.sql
+++ whose_bestfriend_earns_better.sql
@@ -1,6 +1,33 @@
 /*
 People whose best friends got better salaries: MySQL
 */
+-- Students
+-- _____________
+-- | id | name |
+-- _____________
+-- |  1 |    A |
+-- |  2 |    B |
+-- |  3 |    C |
+-- |  4 |    D |
+-- _____________
+-- Friends
+-- __________________
+-- | id | friend_id |
+-- __________________
+-- |  1 |         4 |
+-- |  2 |         3 |
+-- |  3 |         2 |
+-- |  4 |         1 |
+-- __________________
+-- Packages
+-- _______________
+-- | id | salary |
+-- _______________
+-- |  1 |     10 |
+-- |  2 |     20 |
+-- |  3 |     30 |
+-- |  4 |     40 |
+-- _______________
 with cte (id, frnd_id, sal, frnd_sal) as (
     select Friends.ID as id, Friends.Friend_ID as frnd_id, Packages_ID.Salary as sal, Packages_Friend_ID.Salary as frnd_sal
     from Friends
