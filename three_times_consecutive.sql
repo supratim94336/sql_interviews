600a330e2127ba380a4587f997613393362fff6c
Supratim Das <supratim.gre2014@yahoo.com>
2022-02-06 18:03:35 +0100

    Update three_times_consecutive.sql

--- three_times_consecutive.sql
+++ three_times_consecutive.sql
@@ -1,3 +1,24 @@
+-- Input: 
+-- Logs table:
+-- +----+-----+
+-- | id | num |
+-- +----+-----+
+-- | 1  | 1   |
+-- | 2  | 1   |
+-- | 3  | 1   |
+-- | 4  | 2   |
+-- | 5  | 1   |
+-- | 6  | 2   |
+-- | 7  | 2   |
+-- +----+-----+
+-- Output: 
+-- +-----------------+
+-- | ConsecutiveNums |
+-- +-----------------+
+-- | 1               |
+-- +-----------------+
+-- Explanation: 1 is the only number that appears consecutively for at least three times.
+
 with t1 as (
     select num,
            lag(num, 1) over(order by id) num1, 
