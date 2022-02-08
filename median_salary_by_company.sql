3c74b48dfa364eeeada226da4c74c6563361ee6d
Supratim Das <supratim.gre2014@yahoo.com>
2022-02-08 10:16:10 +0100

    Create median_salary_by_company.sql

---
+++ median_salary_by_company.sql
@@ -0,0 +1,23 @@
+SELECT
+    id,
+    company, 
+    salary
+FROM (
+    SELECT
+        id,
+        company, 
+        salary,
+        ROW_NUMBER() OVER (PARTITION BY salary, company ORDER BY salary, company) as rn
+    FROM (
+        SELECT
+            id,
+            company, 
+            salary, 
+            ROW_NUMBER() OVER (PARTITION BY company ORDER BY salary ASC, company) AS ROWASC, 
+            ROW_NUMBER() OVER (PARTITION BY company ORDER BY salary DESC) AS ROWDESC 
+        FROM Employee
+    ) X 
+    WHERE ROWASC IN (ROWDESC, ROWDESC - 1, ROWDESC + 1)
+) tmp
+WHERE rn = 1
+ORDER BY company, salary;
