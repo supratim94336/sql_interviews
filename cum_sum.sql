37b085999f0d0eb6f91b33ca7b8ead64e82ad5ab
Supratim Das <supratim.gre2014@yahoo.com>
2022-02-17 12:14:46 +0100

    Update cum_sum.sql

--- cum_sum.sql
+++ cum_sum.sql
@@ -1,5 +1,30 @@
-select t1.id, t1.num, SUM(t2.num) as sum
-from table t1
-inner join table t2 on t1.id >= t2.id
-group by t1.id, t1.num
-order by t1.id
+-- Input: 
+-- Activity table:
+-- +-----------+-----------+------------+--------------+
+-- | player_id | device_id | event_date | games_played |
+-- +-----------+-----------+------------+--------------+
+-- | 1         | 2         | 2016-03-01 | 5            |
+-- | 1         | 2         | 2016-05-02 | 6            |
+-- | 1         | 3         | 2017-06-25 | 1            |
+-- | 3         | 1         | 2016-03-02 | 0            |
+-- | 3         | 4         | 2018-07-03 | 5            |
+-- +-----------+-----------+------------+--------------+
+-- Output: 
+-- +-----------+------------+---------------------+
+-- | player_id | event_date | games_played_so_far |
+-- +-----------+------------+---------------------+
+-- | 1         | 2016-03-01 | 5                   |
+-- | 1         | 2016-05-02 | 11                  |
+-- | 1         | 2017-06-25 | 12                  |
+-- | 3         | 2016-03-02 | 0                   |
+-- | 3         | 2018-07-03 | 5                   |
+-- +-----------+------------+---------------------+
+
+-- games played so far
+select t1.player_id, t1.event_date, SUM(t2.games_played) as games_played_so_far
+from Activity t1
+inner join Activity t2
+    on t1.player_id = t2.player_id
+    and t1.event_date >= t2.event_date
+group by t1.player_id, t1.event_date
+order by t1.player_id;
